f8ba0c3154695d99d71a1478e73e9984d9fc6fda
Hackbright Student <user@nuc02.(none)>
2014-02-04 12:38:02 -0800

    Getting a Random Number

--- subl guessing.py
+++ subl guessing.py
@@ -1,2 +1,6 @@
+import random
+
 print "Hey! What is your name?"
 name = raw_input("> ")
+print "%s, I'm thinking of a number between 1 and 100. Try to guess my number." % name
+number = random.randint(0, 100)
