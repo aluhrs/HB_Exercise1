939a9b67dcb90676e0cadc86923efd632768fd54
Hackbright Student <user@nuc02.(none)>
2014-02-04 15:31:05 -0800

    Removed redundant code

--- subl guessing.py
+++ subl guessing.py
@@ -1,29 +1,27 @@
 import random
 
-i = 1
-num_guesses = []
 
 print "Hey! What is your name?"
 name = raw_input("> ")
 print "%s, I'm thinking of a number between 1 and 100. Try to guess my number." % name
-number = random.randint(0, 100)
+number = random.randint(1, 101)
 
 print "Your guess?"
 guess = int(raw_input("> "))
 
+i = 1
+
 while True:
     if guess > number:
         print "Your guess is too high: try again."
-        num_guesses.append(i)
-        i = i + 1
-        print "Your guess?"
-        guess = int(raw_input("> "))
+
     elif guess < number:
         print "Your guess is too low: try again."
-        num_guesses.append(i)
-        i = i + 1
-        print "Your guess?"
-        guess = int(raw_input("> "))
+        
     else:
         print "Well done, %s! You found my number in %i tries!" % (name, i)
         break
+
+    i = i + 1
+    print "Your guess?"
+    guess = int(raw_input("> "))
