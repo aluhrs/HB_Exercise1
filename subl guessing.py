b69ee10b63cb50c3676a48cf2b93aa67b263b8e9
Hackbright Student <user@nuc02.(none)>
2014-02-04 12:56:46 -0800

    We have guessed the number: No Congrats

--- subl guessing.py
+++ subl guessing.py
@@ -4,3 +4,19 @@ print "Hey! What is your name?"
 name = raw_input("> ")
 print "%s, I'm thinking of a number between 1 and 100. Try to guess my number." % name
 number = random.randint(0, 100)
+
+print "Your guess?"
+guess = int(raw_input("> "))
+
+while True:
+    if guess > number:
+        print "Your guess is too high: try again."
+        print "Your guess?"
+        guess = int(raw_input("> "))
+    elif guess < number:
+        print "Your guess is too low: try again."
+        print "Your guess?"
+        guess = int(raw_input("> "))
+    else:
+        print "Well done, %s! You found my number!" % name
+        return False
