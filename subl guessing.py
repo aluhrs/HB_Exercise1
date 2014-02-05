26cabcc2d4ac5156a9d45bbb0eb9c9c825fdc657
Hackbright Student <user@nuc02.(none)>
2014-02-04 16:04:25 -0800

    Added Error Checking

--- subl guessing.py
+++ subl guessing.py
@@ -6,22 +6,20 @@ name = raw_input("> ")
 print "%s, I'm thinking of a number between 1 and 100. Try to guess my number." % name
 number = random.randint(1, 101)
 
-print "Your guess?"
-guess = int(raw_input("> "))
-
-i = 1
 
 while True:
-    if guess > number:
-        print "Your guess is too high: try again."
 
-    elif guess < number:
+    print "Your guess?"
+    guess = raw_input("> ")
+    i = 1
+    if not guess.isdigit():
+        print "That's not an int! Try again!"
+    elif int(guess) > number:
+        print "Your guess is too high: try again."
+    elif int(guess) < number:
         print "Your guess is too low: try again."
-        
     else:
         print "Well done, %s! You found my number in %i tries!" % (name, i)
         break
 
     i = i + 1
-    print "Your guess?"
-    guess = int(raw_input("> "))
